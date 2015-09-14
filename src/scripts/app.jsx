63f45c3ab0365fd0d4dacb583ff8a70ca1087d49
Luke Horvat <lukehorvat@gmail.com>
2015-09-14 19:28:19 +1000

    Remove curtains

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -1,13 +1,11 @@
 import React from "react/addons";
 import {Header} from "./header";
 import {Content} from "./content";
-import {Curtains} from "./curtains";
 
 class App extends React.Component {
   render() {
     return (
       <div id="app">
-        <Curtains />
         <Header />
         <Content />
       </div>
